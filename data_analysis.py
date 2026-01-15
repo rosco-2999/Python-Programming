cd82955cecfc5a36de135f6633a3dfd52a0dff4f
rosco-2999 <robinson1589@student.faytechcc.edu>
2026-01-15 19:16:11 +0000

    very basic data analysis (min max)

--- data_analysis.py
+++ data_analysis.py
@@ -1,1 +1,23 @@
 # Noah Robinson
+
+import pandas as pd
+
+# Read the csv file into a variable 
+df = pd.read_csv("best_selling_albums.csv")
+
+# Display the first few rows in the dataset
+print(df.head(10))
+
+# Put the Sales_Millions column into a variable
+sales = df["Sales_Millions"]
+print()
+# Display largest sales number
+# print(sales.max())
+
+largest_sales = df[df["Sales_Millions"] == df['Sales_Millions'].max()]
+print("Largest Number of Recorded Sales")
+print()
+print(largest_sales)
+
+# Display smallest sales number
+print(sales.min())
